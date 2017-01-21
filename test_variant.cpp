624ebb788a25e5b14aca7911816eb7d6584b62b1
Marcel Birn <marcelbirn@gmx.de>
2017-01-21 15:42:26 +0100

    Adding file variant3.h

---
+++ test_variant.cpp
@@ -0,0 +1,320 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "variant3.h"
+#include "../doctest/doctest/doctest.h"
+#include <iostream>
+
+using namespace mb;
+
+enum class CID
+{
+    INVALID,
+    DEFAULT,
+    VALUE,
+    LVALUE_COPY,
+    RVALUE_Copy
+};
+
+std::ostream& operator<<(std::ostream &s, const CID &v)
+{
+    switch(v)
+    {
+        case CID::INVALID: s << "CID::INVALID"; break;
+        case CID::DEFAULT: s << "CID::DEFAULT"; break;
+        case CID::VALUE: s << "CID::VALUE"; break;
+        case CID::LVALUE_COPY: s << "CID::LVALUE_COPY"; break;
+        case CID::RVALUE_Copy: s << "CID::RVALUE_Copy"; break;
+    }
+
+    return s;
+}
+
+struct TestClass
+{
+    TestClass()
+    : constructorid{CID::DEFAULT}
+    {}
+
+    TestClass(int a)
+    : a(a)
+    , constructorid{CID::VALUE}
+    {}
+
+    TestClass(const TestClass &other)
+    : a(other.a)
+    , constructorid{CID::LVALUE_COPY}
+    {}
+
+    TestClass(TestClass &&other)
+    : a(other.a)
+    , constructorid(CID::RVALUE_Copy)
+    {}
+
+    ~TestClass()
+    {}
+
+    bool operator==(const TestClass &rhs) const
+    {
+        return a == rhs.a;
+    }
+
+    int a = 10;
+
+    // statistic members
+    CID constructorid = CID::INVALID;
+};
+
+std::ostream& operator<<(std::ostream &s, const TestClass &t)
+{
+    s << "TestClass{" << t.a << "}";
+    return s;
+}
+
+struct TestClass2
+{
+    operator TestClass&&()
+    {
+        return std::move(TestClass{42});
+    }
+};
+
+template <typename T>
+void printer1(T&)
+{
+    std::cout << "printer1 - lvalue" << std::endl;
+}
+
+template <typename T>
+void printer1(const T&)
+{
+    std::cout << "printer1 - const lvalue" << std::endl;
+}
+
+template <typename T>
+void printer1(T &&)
+{
+    std::cout << "printer1 - rvalue" << std::endl;
+}
+
+template <typename T>
+void printer2(T &&v)
+{
+    std::cout << "printer 2 - ";
+    printer1(std::forward<T>(v));
+}
+
+template <typename T>
+struct Test
+{
+    Test(T&)
+    {
+        std::cout << "Test - lvalue" << std::endl;
+    }
+
+    Test(const T&)
+    {
+        std::cout << "Test - const lvalue" << std::endl;
+    }
+
+    Test(T&&)
+    {
+        std::cout << "Test - rvalue" << std::endl;
+    }
+
+    template <typename U>
+    Test(const U&)
+    {
+        std::cout << "template Test - const lvalue" << std::endl;
+    }
+
+    template <typename U>
+    Test(U&&)
+    {
+        std::cout << "template Test - rvalue" << std::endl;
+    }
+};
+
+
+struct TestNoDefConstructor
+{
+    TestNoDefConstructor() = delete;
+};
+
+
+
+/*TEST_CASE("Playing around")
+{
+    variant<double, int> a;
+    std::cout << "get<0>(a): " << get<0>(a) << std::endl;
+    CHECK(get<double>(a) == 0.);
+
+    int b = 10;
+    printer1(b);
+    printer1(static_cast<const int&>(b));
+    printer1(int{});
+    printer2(int{});
+
+    Test<int>{b};
+    Test<int>{static_cast<const int&>(b)};
+    Test<int>{int{} };
+
+    double d = 3.;
+    Test<int>{d};
+    Test<int>{static_cast<const double &>(d)};
+    Test<int>{double{}};
+
+}*/
+
+
+TEST_CASE("testing variant default constructor")
+{
+    variant<int, TestClass> a;
+    CHECK(a.index() == 0u);
+    CHECK(get<0>(a) == int{});
+    CHECK(get<int>(a) == int{});
+
+    variant<TestClass, int> b;
+    CHECK(a.index() == 0u);
+    CHECK(get<0>(b) == TestClass{});
+    CHECK(get<TestClass>(b) == TestClass{});
+    CHECK(get<TestClass>(b).constructorid == CID::DEFAULT);
+}
+
+
+TEST_CASE("testing const lvalue constructor")
+{
+    int lval_a{12};
+    variant<int, TestClass> a(lval_a);
+    CHECK(a.index() == 0u);
+    CHECK(get<0u>(a) == lval_a);
+
+    double lval_b{6.};
+    variant<int, TestClass, double> b(lval_b);
+    CHECK(b.index() == 2);
+    CHECK(get<2>(b) == lval_b);
+
+    TestClass lval_c{42};
+    variant<int, TestClass, double> c(lval_c);
+    CHECK(c.index() == 1);
+    CHECK(get<1>(c) == lval_c);
+    CHECK(get<TestClass>(c).constructorid == CID::LVALUE_COPY);
+}
+
+
+TEST_CASE("testing rvalue constructor")
+{
+    variant<int, TestClass> a(TestClass{42});
+    CHECK(a.index() == 1u);
+    CHECK(get<1>(a) == TestClass{42});
+    CHECK(get<1>(a).constructorid == CID::RVALUE_Copy);
+
+    variant<TestClass, int> b(TestClass{42});
+    CHECK(b.index() == 0u);
+    CHECK(get<0>(b) == TestClass{42});
+    CHECK(get<0>(b).constructorid == CID::RVALUE_Copy);
+
+    variant<TestClass, double> c(TestClass2{});
+    CHECK(c.index() == 0u);
+    CHECK(get<0>(c).a == 42);
+    CHECK(get<0>(c).constructorid == CID::RVALUE_Copy);
+
+    variant<int, TestClass, double> d(TestClass2{});
+    CHECK(d.index() == 1u);
+    CHECK(get<1u>(d).a == 42);
+    CHECK(get<1u>(d).constructorid == CID::RVALUE_Copy);
+
+    variant<int, double, TestClass> e(TestClass2{});
+    CHECK(e.index() == 2u);
+    CHECK(get<2u>(e).a == 42);
+    CHECK(get<2u>(e).constructorid == CID::RVALUE_Copy);
+
+    variant<double, TestClass, int> f(TestClass{42});
+    CHECK(f.index() == 1u);
+    CHECK(get<1>(f) == TestClass{42});
+    CHECK(get<1>(f).constructorid == CID::RVALUE_Copy);
+
+    variant<double, int, TestClass> g(TestClass{42});
+    CHECK(g.index() == 2u);
+    CHECK(get<2>(g) == TestClass{42});
+    CHECK(get<2>(g).constructorid == CID::RVALUE_Copy);
+}
+
+
+TEST_CASE("testing variant_index")
+{
+    struct NotConvertible{};
+    CHECK((variant_index_v<int, int>) == 0);
+    CHECK((variant_index_v<int, double, int>) == 1);
+    CHECK((variant_index_v<int, int, double>) == 0);
+    CHECK((variant_index_v<int, double, NotConvertible>) == 0);
+    CHECK((variant_index_v<int, NotConvertible, double>) == 1);
+    CHECK((variant_index_v<int, int, double, unsigned>) == 0);
+    CHECK((variant_index_v<int, double, int, unsigned>) == 1);
+    CHECK((variant_index_v<int, double, unsigned, int>) == 2);
+
+    CHECK((variant_index_v<TestClass2, double, TestClass>) == 1);
+
+    CHECK((variant_index_v<int&, double, unsigned, int>) == 2);
+    CHECK((variant_index_v<double&, double, unsigned, int>) == 0);
+    CHECK((variant_index_v<unsigned&, double, unsigned, int>) == 1);
+};
+
+template <typename T, typename U>
+bool is_same_v = std::is_same<T, U>::value;
+
+TEST_CASE("testing CaptureObject")
+{
+    struct DummyT {};
+
+    using ct1 = decltype(CaptureObject<DummyT, int>{0}.get());
+    CHECK((is_same_v<ct1, int>));
+
+    using ct2 = decltype(CaptureObject<DummyT, int&>{0}.get());
+    CHECK((is_same_v<ct2, int&>));
+
+    using ct3 = decltype(CaptureObject<DummyT, int&&>{0}.get());
+    CHECK((is_same_v<ct3, int&&>));
+
+    using ct4 = decltype(CaptureObject<DummyT, const int>{0}.get());
+    CHECK((is_same_v<ct4, int>)); // that's ok
+
+    using ct5 = decltype(CaptureObject<DummyT, const int&>{0}.get());
+    CHECK((is_same_v<ct5, const int&>));
+
+    using ct6 = decltype(CaptureObject<DummyT, const int&&>{0}.get());
+    CHECK((is_same_v<ct6, const int&&>));
+
+    using ct7 = decltype(CaptureObject<DummyT, int*>{nullptr}.get());
+    CHECK((is_same_v<ct7, int*>));
+
+    using ct8 = decltype(CaptureObject<DummyT, const int*>{nullptr}.get());
+    CHECK((is_same_v<ct8, const int*>));
+
+    using ct9 = decltype(CaptureObject<DummyT, int const*>{nullptr}.get());
+    CHECK((is_same_v<ct9, const int*>));
+}
+
+TEST_CASE("testing variant_alternative")
+{
+    CHECK((is_same_v<variant_alternative_t<0, double>, double>));
+    CHECK((is_same_v<variant_alternative_t<0, double, int>, double>));
+    CHECK((is_same_v<variant_alternative_t<1, double, int>, int>));
+    CHECK((is_same_v<variant_alternative_t<0, double, int, unsigned>, double>));
+    CHECK((is_same_v<variant_alternative_t<1, double, int, unsigned>, int>));
+    CHECK((is_same_v<variant_alternative_t<2, double, int, unsigned>, unsigned>));
+}
+
+//TEST_CASE("testing variant copy constructor")
+//{
+//    variant<int, double> as{3.3};
+//    auto at = as;
+//    CHECK(at.index() == 1u);
+//    CHECK(get<1u>(at) == 3.3);
+//
+//    variant<TestClass, int, double> bs{TestClass(31)};
+//    auto bt = bs;
+//    CHECK(bt.index() == 0u);
+//    CHECK(get<0u>(bt).a == 31);
+//    CHECK(get<0u>(bt).constructorid == CID::LVALUE_COPY);
+//}
+
+
---
+++ variant3.h
@@ -0,0 +1,385 @@
+#ifndef _INCLUDE_VARIANT_H_
+#define _INCLUDE_VARIANT_H_
+
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+//#include "max_sizeof2.h"
+#include <cassert> // todo remove
+
+namespace mb
+{
+
+template <std::size_t C, typename T, typename... Ts>
+struct variant_alternative
+{
+    using type = typename variant_alternative<C-1, Ts...>::type;
+};
+
+template <typename T, typename... Ts>
+struct variant_alternative<0, T, Ts...>
+{
+    using type = T;
+};
+
+template <std::size_t I, typename... Types>
+using variant_alternative_t = typename variant_alternative<I, Types...>::type;
+
+template <typename T>
+struct is_destructable
+{
+    static constexpr bool value = std::is_class<T>::value || std::is_union<T>::value;
+};
+
+
+template <typename T, typename... Ts>
+struct variant_index
+{
+private:
+    template <typename SOURCE_T, typename TARGET_T>
+    struct convert_type
+    {
+    public:
+        static constexpr unsigned SAME = 1;
+        static constexpr unsigned CONVERTIBLE = 3;
+        static constexpr unsigned NOT_CONVERTIBLE = 4;
+
+    private:
+        struct ONE {char a[1];};
+        struct TWO {char a[3];};
+        struct THREE {char a[4];};
+
+        template <typename So, typename Ta>
+        struct helper
+        {
+            static constexpr TWO func(Ta);
+            static constexpr THREE func(...);
+
+            static constexpr unsigned value = sizeof(func(So{}));
+        };
+
+        template <typename So>
+        struct helper<So, So>
+        {
+            static constexpr unsigned value = SAME;
+        };
+
+    public:
+        static constexpr unsigned value = helper<SOURCE_T, TARGET_T>::value;
+    };
+
+    template <typename SOURCE_T, typename TARGET_T>
+    static constexpr unsigned convert_type_v = convert_type<SOURCE_T, TARGET_T>::value;
+
+
+    template <
+        typename U,
+        std::size_t POS,
+        std::size_t SEL_POS,
+        unsigned TYPE,
+        typename V,
+        typename... Vs>
+    struct get_index_helper
+    {
+        static constexpr unsigned cur_convert_type = convert_type_v<U, V>;
+        static_assert(TYPE != convert_type<U, V>::SAME
+                || cur_convert_type != convert_type<U, V>::SAME, "Ambiguous types!");
+        static constexpr unsigned new_type =
+            cur_convert_type < TYPE ? cur_convert_type :
+            cur_convert_type == TYPE && TYPE == convert_type<U, V>::CONVERTIBLE ? 2 : TYPE;
+        static constexpr std::size_t new_pos = new_type < TYPE ? POS : SEL_POS;
+        static constexpr std::size_t value =
+            get_index_helper<U, POS+1, new_pos, new_type, Vs...>::value;
+    };
+
+    template <typename U, std::size_t POS, std::size_t SEL_POS, unsigned TYPE, typename V>
+    struct get_index_helper<U, POS, SEL_POS, TYPE, V>
+    {
+        static constexpr unsigned cur_convert_type = convert_type_v<U, V>;
+        static_assert(TYPE != convert_type<U, V>::SAME
+                || cur_convert_type != convert_type<U, V>::SAME, "Ambiguous types!");
+        static constexpr unsigned new_type =
+            cur_convert_type < TYPE ? cur_convert_type :
+            cur_convert_type == TYPE && TYPE == convert_type<U, V>::CONVERTIBLE ? 2 : TYPE;
+        static_assert(new_type != 2, "Ambiguous types!");
+        static_assert(new_type < convert_type<U, V>::NOT_CONVERTIBLE, "No matching type!");
+        static constexpr std::size_t value = new_type < TYPE ? POS : SEL_POS;
+    };
+public:
+    static constexpr std::size_t value =
+        get_index_helper<
+            std::remove_reference_t<T>,
+            0,
+            0,
+            convert_type<T, T>::NOT_CONVERTIBLE,
+            Ts...>::value;
+};
+
+template <typename T, typename... Ts>
+constexpr std::size_t variant_index<T, Ts...>::value;
+
+template <typename T, typename... Ts>
+static constexpr std::size_t variant_index_v = variant_index<T, Ts...>::value;
+
+std::size_t variant_npos = -1;
+
+template <typename T, typename U>
+struct CaptureObject
+{
+    CaptureObject(std::remove_reference_t<U> &&o)
+    : o(std::move(o))
+    {}
+
+    constexpr U get()
+    {
+        return std::forward<U>(o);
+    }
+
+    std::remove_reference_t<U> &&o;
+};
+
+
+template <typename... Ts>
+union variant_data;
+
+template <typename T, typename... Rest>
+union variant_data<T, Rest...>
+{
+    variant_data()
+    : val{}
+    {}
+
+    variant_data(const T &val)
+    : val(val)
+    {}
+
+    variant_data(const variant_data<Rest...> &other_rest, std::size_t i)
+    : rest(other_rest, i)
+    {}
+
+//    variant_data(const variant_data &other, std::size_t i)
+//    : variant_data(i==0 ? variant_data(other.val) : variant_data(other.rest, i-1))
+//    {}
+
+    template <typename U>
+    constexpr variant_data(CaptureObject<T, U> &&v)
+    : val(v.get())
+    {}
+
+    template <typename U, typename V>
+    constexpr variant_data(CaptureObject<U, V> &&v)
+    : rest(std::move(v))
+    {}
+
+    ~variant_data()
+    {}
+
+    void operator=(const T &v)
+    {
+        val = v;
+    }
+
+    template <typename U>
+    void operator=(const U &v)
+    {
+        rest = v;
+    }
+
+    void destroy(const T*)
+    {
+        if (is_destructable<T>::value)
+        {
+            val.~T();
+        }
+    }
+
+    template <typename U>
+    void destroy(const U*)
+    {
+        rest.destroy(static_cast<U*>(nullptr));
+    }
+
+    T val;
+    variant_data<Rest...> rest;
+};
+
+template <typename T>
+union variant_data<T>
+{
+    variant_data()
+    : val{}
+    {}
+
+    variant_data(const variant_data &other, std::size_t)
+    : val(other.val)
+    {}
+
+    template <typename U>
+    constexpr variant_data(CaptureObject<T, U> &&v)
+    : val(v.get())
+    {}
+
+    ~variant_data()
+    {}
+
+    void operator=(const T &v)
+    {
+        val = v;
+    }
+
+
+    void destroy(const T*)
+    {
+        if (is_destructable<T>::value)
+        {
+            val.~T();
+        }
+    }
+
+    T val;
+};
+
+template <typename... Ts> class variant;
+
+template <typename T, typename... Ts> constexpr T& get(variant<Ts...> &v);
+
+template <typename... Ts>
+class variant
+{
+private:
+    typedef void (*DestructorT)(variant<Ts...>&);
+    static const DestructorT destructors[sizeof...(Ts)];
+
+    template <typename T>
+    static void destroy(variant<Ts...> &v)
+    {
+        if (is_destructable<T>::value)
+        {
+            v.data.destroy(static_cast<T*>(nullptr));
+            // get<T>(v).~T();
+        }
+    }
+
+    template <std::size_t pos, typename U, typename... Us>
+    struct get_helper_pos
+    {
+        static constexpr variant_alternative_t<pos, U, Us...> const&
+            get(const variant_data<U, Us...> &a)
+        {
+            return get_helper_pos<pos-1, Us...>::get(a.rest);
+        }
+
+        static constexpr variant_alternative_t<pos, U, Us...>&
+            get(variant_data<U, Us...> &a)
+        {
+            return get_helper_pos<pos-1, Us...>::get(a.rest);
+        }
+    };
+
+    template <typename U, typename... Us>
+    struct get_helper_pos<0, U, Us...>
+    {
+        static constexpr U const& get(const variant_data<U, Us...> &a)
+        {
+            return a.val;
+        }
+
+        static constexpr U& get(variant_data<U, Us...> &a)
+        {
+            return a.val;
+        }
+    };
+
+public:
+    variant()
+    {
+        _index = 0;
+    }
+
+    variant(variant &other)
+    : variant(static_cast<const variant&>(other))
+    {}
+
+//    variant(const variant &other)
+//    : data(other.data, other.index())
+//    {
+//        _index = other.index();
+//    }
+
+    template <typename U>
+    constexpr variant(U &&v)
+    : data(CaptureObject<variant_alternative_t<variant_index_v<U, Ts...>, Ts...>,
+                         U>(std::move(v)))
+    {
+        _index = variant_index_v<std::remove_reference_t<U>, Ts...>;
+    }
+
+    ~variant()
+    {
+        if (_index < sizeof...(Ts))
+            destructors[_index](*this);
+    }
+
+    template <typename U>
+    variant<Ts...>& operator=(const U &v)
+    {
+        const auto old_index = _index;
+        _index = variant_npos;
+
+        if (old_index < sizeof...(Ts))
+            destructors[old_index](*this);
+
+        data = v;
+        _index = variant_index_v<U, Ts...>;
+        return *this;
+    }
+
+    constexpr std::size_t index() const noexcept
+    {
+        return _index;
+    }
+
+private:
+    template <std::size_t pos, typename... Us>
+    friend constexpr variant_alternative_t<pos, Us...> const& get(const variant<Us...>&);
+
+    template <std::size_t pos, typename... Us>
+    friend constexpr variant_alternative_t<pos, Us...>& get(variant<Us...>&);
+
+    std::size_t _index = variant_npos;
+    variant_data<Ts...> data;
+};
+
+template <typename... Ts>
+const typename variant<Ts...>::DestructorT variant<Ts...>::destructors[sizeof...(Ts)] =
+    {variant<Ts...>::destroy<Ts>...};
+
+
+template <std::size_t pos, typename... Ts>
+constexpr variant_alternative_t<pos, Ts...> const& get(const variant<Ts...> &v)
+{
+    return variant<Ts...>::template get_helper_pos<pos, Ts...>::get(v.data);
+}
+
+template <std::size_t pos, typename... Ts>
+constexpr variant_alternative_t<pos, Ts...>& get(variant<Ts...> &v)
+{
+    return variant<Ts...>::template get_helper_pos<pos, Ts...>::get(v.data);
+}
+
+template <typename T, typename... Ts>
+constexpr T const& get(const variant<Ts...> &v)
+{
+    return get<variant_index_v<T, Ts...>>(v);
+}
+
+template <typename T, typename... Ts>
+constexpr T& get(variant<Ts...> &v)
+{
+    return get<variant_index_v<T, Ts...>>(v);
+}
+
+
+} //  namesapce mb
+
+#endif
